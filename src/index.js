c2ff62b0b563fce7cc5118a90e5a63e2c08497fc
RL Brown <rlbrown72@gmail.com>
2018-03-12 17:14:52 -0400

    add cors

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 import express from 'express';
+import cors from 'cors';
+
 import { howardRouter } from './routes/howard-router';
 
 require('dotenv').config();
@@ -6,6 +8,7 @@ require('dotenv').config();
 const port = process.env.PORT;
 const app = express();
 
+app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use('/howard', howardRouter);
 
