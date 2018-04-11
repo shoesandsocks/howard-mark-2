9de73e9371b3153d17b56a4f83093fe6911b9a96
RL Brown <rlbrown72@gmail.com>
2018-04-11 16:07:26 -0400

    tweak index to serve client & ADD CLIENT

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import express from 'express';
+import path from 'path';
 import cors from 'cors';
-
 import { howardRouter } from './routes/howard-router';
 
 require('dotenv').config();
@@ -10,6 +10,11 @@ const app = express();
 
 app.use(cors());
 app.use(express.urlencoded({ extended: true }));
+
+/* requests to /howard go through routes/howardRouter */
 app.use('/howard', howardRouter);
+
+/* catch-all sends everything to build/index.html (front-end, built elsewhere/pasted here) */
+app.use(express.static(path.join(__dirname, '../client/build')));
 
 app.listen(port, () => `On ${port}`);
