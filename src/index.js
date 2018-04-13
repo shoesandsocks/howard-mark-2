6613649d92ad68d11f91dabd9f558c753e88b1ad
RL Brown <rlbrown72@gmail.com>
2018-04-13 14:05:11 -0400

    add sslify

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import path from 'path';
 import cors from 'cors';
+import enforce from 'express-sslify';
 import { howardRouter } from './routes/howard-router';
 
 require('dotenv').config();
@@ -8,9 +9,24 @@ require('dotenv').config();
 const port = process.env.PORT;
 const app = express();
 
-/* basic cors usage to allow other domains to hit /howard. urlencoded to read POST data coming in */
+/*
+* BEGIN SETUP
+*/
+
+/* basic cors usage to allow other domains to hit /howard. TODO: maybe revisit this */
 app.use(cors());
+
+/* urlencoded to read POST data coming in */
 app.use(express.urlencoded({ extended: true }));
+
+/* force HTTPS
+*  docs say: "Use enforce.HTTPS({ trustProtoHeader: true }) in case you are behind
+// a load balancer (e.g. Heroku)."" */
+app.use(enforce.HTTPS({ trustProtoHeader: true }));
+
+/*
+* BEGIN ROUTES
+*/
 
 /* requests to /howard go through routes/howardRouter */
 app.use('/howard', howardRouter);
