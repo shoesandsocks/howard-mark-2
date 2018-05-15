bcfc6b817cf23186e97b207f9b3d87d86e168982
RL Brown <rlbrown72@gmail.com>
2018-05-15 15:27:26 -0400

    data importer and big changes to frontend css

--- src/index.js
+++ src/index.js
@@ -10,6 +10,7 @@ import { howardRouter } from './routes/howard-router';
 import { howardSlackRouter } from './routes/howard-slack-router';
 import { cronRouter } from './routes/cron-router';
 import { authRouter } from './routes/auth-router';
+import { updateRouter } from './routes/update-router';
 
 import { runJobs } from './utils/cron-management';
 import { runBot, stopBot } from './utils/slack-responder';
@@ -100,6 +101,7 @@ app.use('/howardslack', howardSlackRouter);
 app.use('/howardcron', isAuthed, cronRouter);
 // app.use('/howardcron', cronRouter);
 app.use('/oauth', authRouter);
+app.use('/howardupdate', isAuthed, updateRouter);
 
 /* next 2 lines send other routes to /client/build (front-end, built in h-m-2-frontend folder) */
 app.use('/', express.static(path.join(__dirname, '../client/build')));
---
+++ src/routes/update-router.js
@@ -0,0 +1,15 @@
+/* eslint-disable no-case-declarations */
+import express from 'express';
+import MongoClient from 'mongodb';
+
+import { coreUpdate } from '../utils/json-to-database-reconciler';
+
+require('dotenv').config();
+
+export const updateRouter = express.Router();
+
+updateRouter.get('/', async (req, res) => {
+  const client = await MongoClient.connect(process.env.MLAB);
+  const db = await client.db('howard');
+  coreUpdate(db).then(r => res.send(r));
+});
