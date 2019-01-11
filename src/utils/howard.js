6abfac1f87c397bf2b508e49d11a3d4ab6719161
RL Brown <rlbrown72@gmail.com>
2019-01-11 12:55:01 -0500

    return obj not string

--- src/utils/howard.js
+++ src/utils/howard.js
@@ -56,7 +56,7 @@ export const howard = async (query, argument) => {
   /* getMarkov(string) returns markov from string seed */
   const getMarkov = (input) => {
     console.log('getting');
-    return 'hi';
+    return { result: 'hi' };
   };
 
   let returnValue;
